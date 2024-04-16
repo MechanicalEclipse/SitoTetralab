9eb3410ef680cc8c717fe354c298db047aceb364
MechanicalEclipse <alessandro.cursoli@gmail.com>
2024-04-17 00:28:00 +0200

    updates

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,8 @@
 <x-layouts.app>
+  
     @livewire('hero')
 
     @livewire('testimonials')
-
+    <div class="m-2 mx-11 border-t border-neutral-300"></div>
     @livewire('features-stats')
 </x-layouts.app>
