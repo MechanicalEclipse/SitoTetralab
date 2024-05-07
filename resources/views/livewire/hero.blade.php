7603632a5768dc90b56e511669dc866d2cb02e21
Mechanical-Eclipse <alessandro.cursoli@gmail.com>
2024-05-07 11:44:05 +0200

    minor update

--- resources/views/livewire/hero.blade.php
+++ resources/views/livewire/hero.blade.php
@@ -1,5 +1,84 @@
 <!-- Hero -->
-<div class="flex max-w-[85rem] mx-auto px-4 sm:px-6 lg:px-8 h-screen items-center align-middle">
+<div>
+
+  {{-- <div class="flex h-2/5 xs:h-2/3">
+    <img class="w-full h-auto object-cover" src="https://images.unsplash.com/photo-1665686376173-ada7a0031a85?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=900&h=700&q=80" alt="Image Description">
+  </div>
+
+  <div class="relative overflow-hidden">
+    <!-- Gradients -->
+    <div aria-hidden="true" class="flex absolute -top-96 start-1/2 transform -translate-x-1/2">
+      <div class="bg-gradient-to-r from-violet-300/50 to-purple-100 blur-3xl w-[25rem] h-[44rem] rotate-[-60deg] transform -translate-x-[10rem]"></div>
+      <div class="bg-gradient-to-tl from-blue-50 via-blue-100 to-blue-50 blur-3xl w-[90rem] h-[50rem] rounded-fulls origin-top-left -rotate-12 -translate-x-[15rem]"></div>
+    </div>
+    <!-- End Gradients -->
+
+    <div class="relative z-10">
+      <div class="max-w-[85rem] mx-auto px-4 sm:px-6 lg:px-8 py-10 lg:py-16">
+        <div class="max-w-2xl text-center mx-auto">
+          <p class="inline-block text-sm font-medium bg-clip-text bg-gradient-to-l from-blue-600 to-violet-500 text-transparent">
+            Preline: A vision for 2023
+          </p>
+
+          <!-- Title -->
+          <div class="mt-5 max-w-2xl">
+            <h1 class="block font-semibold text-gray-800 text-4xl md:text-5xl lg:text-6xl">
+              The Intuitive Web Solutions
+            </h1>
+          </div>
+          <!-- End Title -->
+
+          <div class="mt-5 max-w-3xl">
+            <p class="text-lg text-gray-600">Preline UI is an open-source set of prebuilt UI components, ready-to-use examples and Figma design system based on the utility-first Tailwind CSS framework.</p>
+          </div>
+
+          <!-- Buttons -->
+          <div class="mt-8 gap-3 flex justify-center">
+            <a class="py-3 px-4 inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none" href="#">
+              Get started
+              <svg class="flex-shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="m9 18 6-6-6-6"/></svg>
+            </a>
+            <a class="py-3 px-4 inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent text-gray-800 hover:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none" href="#">
+              <svg class="flex-shrink-0 size-4" width="19" height="18" viewBox="0 0 19 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M6.875 18C8.531 18 9.875 16.656 9.875 15V12H6.875C5.219 12 3.875 13.344 3.875 15C3.875 16.656 5.219 18 6.875 18Z" fill="#0ACF83"></path>
+                <path d="M3.875 9C3.875 7.344 5.219 6 6.875 6H9.875V12H6.875C5.219 12 3.875 10.656 3.875 9Z" fill="#A259FF"></path>
+                <path d="M3.875 3C3.875 1.344 5.219 0 6.875 0H9.875V6H6.875C5.219 6 3.875 4.656 3.875 3Z" fill="#F24E1E"></path>
+                <path d="M9.87501 0H12.875C14.531 0 15.875 1.344 15.875 3C15.875 4.656 14.531 6 12.875 6H9.87501V0Z" fill="#FF7262"></path>
+                <path d="M15.875 9C15.875 10.656 14.531 12 12.875 12C11.219 12 9.87501 10.656 9.87501 9C9.87501 7.344 11.219 6 12.875 6C14.531 6 15.875 7.344 15.875 9Z" fill="#1ABCFE"></path>
+              </svg>
+              Preline Figma
+            </a>
+          </div>
+          <!-- End Buttons -->
+        </div>
+      </div>
+    </div>
+
+  </div> --}}
+  <section>
+    <div class="bg-gradient-to-b from-curious-blue via-curious-blue-300">
+      <div class="container flex flex-col items-center px-4 py-16 pb-24 mx-auto text-center lg:pb-56 md:py-32 md:px-10 lg:px-32 text-slate-800">
+        <h1 class="text-5xl font-bold leading-none sm:text-6xl xl:max-w-4xl ">Analisi e consulenza ambientale</h1>
+        <p class="mt-6 mb-8 text-2xl sm:mb-12 xl:max-w-7xl">Siamo l’alleato di fiducia per le imprese che ricercano un punto di riferimento che possa affiancarle, con attenzione ed efficacia, nella gestione degli adempimenti ambientali e della salute e sicurezza nei luoghi di lavoro.</p>
+        <div class="flex flex-wrap justify-center">
+          <div class="w-full mt-5 lg:mt-8 flex flex-row items-center gap-2 sm:flex-row sm:gap-3">
+            <a class="sm:w-auto py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-jaffa text-white hover:bg-jaffa-600 disabled:opacity-50 disabled:pointer-events-none" href="#">
+              Scopri di più
+            </a>
+            <a class="sm:w-auto py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-curious-blue text-white hover:bg-curious-blue-700 disabled:opacity-50 disabled:pointer-events-none" href="#">
+              Contattaci
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="lg:h-3/4 sm:h-100">
+      <img src="https://source.unsplash.com/random/480x320" alt="" class="w-5/6 h-full mx-auto mb-12 -mt-20 dark:bg-gray-500 rounded-lg shadow-md lg:-mt-40">
+    </div>
+  </section>
+
+</div>
+{{-- <div class="flex max-w-[85rem] mx-auto px-4 sm:px-6 lg:px-8 h-screen items-center align-middle">
     <!-- Grid -->
     <div class="grid lg:grid-cols-7 lg:gap-x-8 h-5/6 xl:gap-x-12 lg:items-center">
       <div class="lg:col-span-3">
@@ -24,6 +103,6 @@
       <!-- End Col -->
     </div>
     <!-- End Grid -->
-  </div>
+  </div> --}}
 <!-- End Hero -->
 
--- resources/views/livewire/services/type/service-type.blade.php
+++ resources/views/livewire/services/type/service-type.blade.php
@@ -1,7 +1,7 @@
 <div>
     @livewire('services.type.header', ['type' => $type])
     <div class="max-w-screen-2xl px-4 xl:px-0  lg:pt-20 lg:pb-20 mx-auto">
-        <div class="pb-12 grid grid-cols-{{(count($services) % 2) == 0 ? 2:3}} gap-4 sm:gap-6 md:gap-8 lg:gap-12">
+        <div class="pb-12 grid lg:grid-cols-{{(count($services) % 2) == 0 ? 2:3}} md:grid-cols-3 grid-cols-1 gap-4 sm:gap-6 md:gap-8 lg:gap-12">
             @foreach ($services as $service)
                 @livewire('services.type.body', ['service' => $service, 'type' => $type])
             @endforeach
